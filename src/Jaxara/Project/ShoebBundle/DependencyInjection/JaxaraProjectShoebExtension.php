988eba39726e918bb5af00d17c1170a36110f9fb
Shoeb Ahmed Sabur <shoeb@jaxara.com>
2013-11-21 16:54:27 +0600

    Creating the code output service - copied from ACME bundle

--- src/Jaxara/Project/ShoebBundle/DependencyInjection/JaxaraProjectShoebExtension.php
+++ src/Jaxara/Project/ShoebBundle/DependencyInjection/JaxaraProjectShoebExtension.php
@@ -3,9 +3,9 @@
 namespace Jaxara\Project\ShoebBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Config\FileLocator;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -22,7 +22,7 @@ class JaxaraProjectShoebExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
     }
 }
